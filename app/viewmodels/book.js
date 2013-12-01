0d50b2cc55aee188e7018ed2e4b686e4a4dad42d
mpsk <m.piskovyi@ukr.net>
2013-12-01 21:45:23 +0200

    Added book contents and find selected section

--- app/viewmodels/book.js
+++ app/viewmodels/book.js
@@ -7,12 +7,14 @@ define(['plugins/router',
     var REST = rest;
 
     var currentBook = ko.observable();
+
     var context = ko.observable('');
-    var contents = ko.observable(['']);
+    var contents = ko.observableArray([]);
+    var $bookSections = [];
 
     var book = {
 
-        getBookContent: function(bookName){
+        getBook: function(bookName){
 
             _.each(user.books(), function(item){
                 if (item.name === bookName) {
@@ -27,16 +29,42 @@ define(['plugins/router',
                 var xml = $.parseXML(text);
                 console.warn(xml);
                 context(text);
+                book.showContents();
             });
 
         },
 
-        showContext: function(){
+        // Оглавление
+        showContents: function(){
+            var xml = $.parseXML(context());
+            var body = $(xml).find('body');
+            var sections = $(body[0]).find('section');
+            console.warn(sections);
+            $bookSections = sections;
 
+            _.each(sections, function(item, i){
+                var title = $(item).find('title').text();
+                title = title.replace(/\n/g, '');
+
+                if (title) {
+                    var section = {title: title, index: i};
+                    contents.push(section);
+                }
+            });
+
+            console.warn(body);
+            console.warn(contents());
+            // console.warn(sections);
         },
 
-        getContents: function(){
-
+        // Текст
+        showContext: function(){
+            var that = this;
+            _.each($bookSections, function(item, i){
+                if (that.index === i) {
+                    console.warn(item);
+                }
+            });
         },
 
         getSelectedText: function(vm, evt){
@@ -52,15 +80,15 @@ define(['plugins/router',
     };
 
     function activate(bookName){
-
         // FIXME: Bad solution. Maybe use trigger app events.
+        
+        contents([]);
+
         var check = setInterval(function(){
             if (user.id) {
-
-                book.getBookContent(bookName);
+                book.getBook(bookName);
                 clearInterval(check);
             }
-
         }, 500);
 
     }
@@ -70,6 +98,7 @@ define(['plugins/router',
         user: user,
         currentBook: currentBook,
         context: context,
+        contents: contents,
         showContext: book.showContext,
         getSelectedText: book.getSelectedText
     };
