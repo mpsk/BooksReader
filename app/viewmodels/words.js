8f210915662f32350d0f0b857369c3a751985f95
mpsk <m.piskovyi@ukr.net>
2013-12-01 21:51:56 +0200

    fix words code

--- app/viewmodels/words.js
+++ app/viewmodels/words.js
@@ -1,30 +1,29 @@
 define(['service/user'], function(user) {
 
     var self = {
-      selectedBook: ko.observable(''),
-      filter: ko.observable(''),
-      filteredWords: ko.computed(function () {
-          return ko.utils.arrayFilter(user.words(), function (words1) {
-             var sstr = words1.text;
-             var bstr = words1.book;
-             var result = true;
-             if(self.filter()!=undefined){
-               result = sstr.indexOf(self.filter())> -1 || !self.filter();                   
-                  if(self.selectedBook()!=undefined){                  
-                    var b_name = self.selectedBook().name;
-                    if(bstr.toString()!=b_name.toString()) result=false;
-                  }
-              }      
-            return result;
-          });
-        
-         
-       }),
-      deleteWord: function(text){
-         //user.words()
-       }
+        selectedBook: ko.observable(''),
+        filter: ko.observable(''),
+        filteredWords: ko.computed(function() {
+            return ko.utils.arrayFilter(user.words(), function(words1) {
+                var sstr = words1.text;
+                var bstr = words1.book;
+                var result = true;
+                if (self.filter() != undefined) {
+                    result = sstr.indexOf(self.filter()) > -1 || !self.filter();
+                    if (self.selectedBook() != undefined) {
+                        var b_name = self.selectedBook().name;
+                        if (bstr.toString() != b_name.toString()) result = false;
+                    }
+                }
+                return result;
+            });
+
+
+        }),
+        deleteWord: function(text) {
+            //user.words()
         }
-    
+    };
    
 
     return {
