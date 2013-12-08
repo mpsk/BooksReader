89a6af21a6baf6134545efd341ab4185205b92dc
mpsk <m.piskovyi@ukr.net>
2013-12-08 16:37:44 +0200

    Fix problem with route to words from any view.

--- app/viewmodels/words.js
+++ app/viewmodels/words.js
@@ -1,25 +1,30 @@
 define(['service/user'], function (user) {
 
+    var selectedBook = ko.observable('');
+    var filter = ko.observable('');
+
     var self = {
-        selectedBook: ko.observable(''),
-        filter: ko.observable(''),
+
         filteredWords: ko.computed(function() {
             return ko.utils.arrayFilter(user.words(), function(words1) {
+
                 var sstr = words1.text;
                 var bstr = words1.book;
                 var result = true;
-                if (self.filter() != undefined) {
-                    result = sstr.indexOf(self.filter()) > -1 || !self.filter();
-                    if (self.selectedBook() != undefined) {
-                        var b_name = self.selectedBook().name;
+
+                if (filter() != undefined) {
+                    result = sstr.indexOf(filter()) > -1 || !filter();
+
+                    if ( (selectedBook() !== '') && (selectedBook() !== undefined) ) {
+                        var b_name = selectedBook().name;
                         if (bstr.toString() != b_name.toString()) result = false;
                     }
                 }
+
                 return result;
             });
-
-
         }),
+        
         deleteWord: function(obj) {
             user.words.remove(this);
         }
@@ -28,9 +33,9 @@ define(['service/user'], function (user) {
 
     return {
        words: user.words,
-       selectedBook: self.selectedBook,
+       selectedBook: selectedBook,
        books: user.books,
-       filter: self.filter,
+       filter: filter,
        //fbId: user.fbId,
        deleteWord: self.deleteWord,
        filteredWords: self.filteredWords
